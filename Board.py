28b0bce8b8d3101d0f307640c95fe6df4c37320e
avidlaud <avidlaud@gmail.com>
2020-11-14 17:23:37 -0500

    Implement new part 2 methods

--- Board.py
+++ Board.py
@@ -11,6 +11,8 @@ CAVE = 3
 FOUND = 1
 MISSING = 0
 
+BLOCK = 999999999 #Arbitrary large value to prevent selection
+
 class Board:
 
     def __init__(self, dim: int, copy_board=None, copy_target=None, moving_target=False):
@@ -28,7 +30,7 @@ class Board:
         self._board_mask[self._board == FOREST] *= 0.3
         self._board_mask[self._board == CAVE] *= 0.1
         if moving_target:
-            self._known_cleared = np.ones((dim, dim))
+            self._known_cleared = np.zeros((dim, dim))
 
     #Not used
     def newTarget(self):
@@ -74,7 +76,7 @@ class Board:
 
     def update_cleared_cells(self) -> None:
         '''Update the cleared cells matrix upon another action'''
-        self._known_cleared = np.maximum(np.ones((self.dim, self.dim)), self._known_cleared - 1)
+        self._known_cleared = np.maximum(np.zeros((self.dim, self.dim)), self._known_cleared - 1)
         return
 
     def exploreMove(self, pos: tuple) -> tuple:
@@ -99,10 +101,18 @@ class Board:
                 neighborhood = [(row, col) for row in range(max(0, pos[0]-5), min(self.dim, pos[0]+6)) for col in range(max(0, pos[1]-5), min(self.dim, pos[1]+6)) if (abs(row-pos[0]) + abs(col-pos[1])) <= 5]
                 for neighbor in neighborhood:
                     row, col = neighbor
-                    self._known_cleared[row][col] = 6 - self.manhattan(neighbor, pos) #Number of turns until the target can walk to the position
+                    self._known_cleared[row][col] = 5 - self.manhattan(neighbor, pos) #Number of turns until the target can walk to the position
                 return (False, False)
             return (False, True)
         return (True, True)
+    
+    def isNearby(self, pos: tuple, radius=5) -> None:
+        '''Restrict search space to cells that are nearby'''
+        far_cells = [(row, col) for row in range(self.dim) for col in range(self.dim) if (abs(row-pos[0]) + abs(col-pos[1])) > radius]
+        for far_cell in far_cells:
+            row, col = far_cell
+            self._known_cleared[row][col] = max(self._known_cleared[row][col], self.manhattan(far_cell, pos)-(radius+1)) #Number of turns until the target can walk to the position
+        return
 
     def getNeighbors(self, pos: tuple) -> list:
         '''returns a list of all valid neighbors given a position on the board'''
@@ -172,7 +182,7 @@ class Board:
 
     def bestContainsMoving(self) -> tuple:
         '''Returns cell with best chance out of cells that are not cleared'''
-        search_board = self.board * (self._known_cleared == 1)
+        search_board = self.board * (self._known_cleared == 0)
         max_pos = search_board.argmax()
         return max_pos//self.dim, max_pos % self.dim
 
@@ -180,6 +190,12 @@ class Board:
         '''returns cell with best chance of finding the target'''
         temp = np.multiply(self.board, self._board_mask)
         max_pos = temp.argmax()
+        return max_pos//self.dim, max_pos % self.dim
+    
+    def bestFindMoving(self) -> tuple:
+        '''Returns cell with best chance out of cells that are not cleared'''
+        search_board = np.multiply(self.board, self._board_mask) * (self._known_cleared == 0)
+        max_pos = search_board.argmax()
         return max_pos//self.dim, max_pos % self.dim
 
     def bestDist(self, pos) -> tuple:
@@ -210,7 +226,7 @@ class Board:
         x_target, y_target = pos
         col_diff, row_diff = np.abs(np.mgrid[-x_target:self.dim-x_target, -y_target:self.dim-y_target])
         distance_mask = col_diff + row_diff + 1
-        distance_mask = distance_mask * np.where(self._known_cleared == 1, self._known_cleared, 99999) #Prevent those which have been cleared from being chosen
+        distance_mask = distance_mask * np.where(self._known_cleared == 0, 1, BLOCK) #Prevent those which have been cleared from being chosen
         scores = np.divide(distance_mask, self.board*self._board_mask)
         min_pos = scores.argmin()
         return min_pos // self.dim, min_pos % self.dim
@@ -266,15 +282,27 @@ class Board:
         #Generate all valid neighbors
         neighborhood = [(row, col) for row in range(max(0, pos[0]-x), min(self.dim, pos[0]+x+1)) for col in range(max(0, pos[1]-x), min(self.dim, pos[1]+x+1)) if (abs(row-pos[0]) + abs(col-pos[1])) <= x]
         return max(neighborhood, key = lambda y: self.board[y[0]][y[1]]) #Return index with max probability
+    
+    def bestLocalMoving(self, pos, x:int) -> tuple:
+        '''Rule 1 implementation - Returns cell with highest chance of containing target within radius x around pos'''
+        #Generate all valid neighbors
+        neighborhood = [(row, col) for row in range(max(0, pos[0]-x), min(self.dim, pos[0]+x+1)) for col in range(max(0, pos[1]-x), min(self.dim, pos[1]+x+1)) if (abs(row-pos[0]) + abs(col-pos[1])) <= x]
+        return max(neighborhood, key = lambda y: self.board[y[0]][y[1]] * (self._known_cleared[y[0]][y[1]] == 0)) #Return index with max probability
 
     def bestLocal2(self, pos, x:int) -> tuple:
         '''Rule 2 implementation - Returns cell with highest chance of finding target within radius x around pos'''
         #Generate all valid neighbors
         neighborhood = [(row, col) for row in range(max(0, pos[0]-x), min(self.dim, pos[0]+x+1)) for col in range(max(0, pos[1]-x), min(self.dim, pos[1]+x+1)) if (abs(row-pos[0]) + abs(col-pos[1])) <= x]
         return max(neighborhood, key = lambda y: self.board[y[0]][y[1]]*self._board_mask[y[0]][y[1]]) #Return index with max probability
+    
+    def bestLocal2Moving(self, pos, x:int) -> tuple:
+        '''Rule 2 implementation - Returns cell with highest chance of finding target within radius x around pos'''
+        #Generate all valid neighbors
+        neighborhood = [(row, col) for row in range(max(0, pos[0]-x), min(self.dim, pos[0]+x+1)) for col in range(max(0, pos[1]-x), min(self.dim, pos[1]+x+1)) if (abs(row-pos[0]) + abs(col-pos[1])) <= x]
+        return max(neighborhood, key = lambda y: self.board[y[0]][y[1]] * self._board_mask[y[0]][y[1]] * (self._known_cleared[y[0]][y[1]] == 0)) #Return index with max probability
 
     def bestLocal3(self, pos, x:int) -> tuple:
         '''Dist rule implementation - Returns cell with highest chance of finding target within radius x around pos'''
         #Generate all valid neighbors
         neighborhood = [(row, col) for row in range(max(0, pos[0]-x), min(self.dim, pos[0]+x+1)) for col in range(max(0, pos[1]-x), min(self.dim, pos[1]+x+1)) if (abs(row-pos[0]) + abs(col-pos[1])) <= x]
-        return min(neighborhood, key = lambda y: (self.manhattan(pos, y)+1)/(self.board[y[0]][y[1]]*self._board_mask[y[0]][y[1]])) #Return index with max probability
+        return min(neighborhood, key = lambda y: ((self.manhattan(pos, y)+1) * (((self._known_cleared[y[0]][y[1]] != 0) * BLOCK) + 1))/(self.board[y[0]][y[1]]*self._board_mask[y[0]][y[1]])) #Return index with max probability
